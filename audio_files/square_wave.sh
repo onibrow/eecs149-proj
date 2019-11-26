45ad212c36f3321f0e761324acfdbaedda862188
onibrow <seiya.ono12@gmail.com>
2019-11-26 20:28:00 +0000

    remove long sons

--- audio_files/square_wave.sh
+++ audio_files/square_wave.sh
@@ -4,14 +4,14 @@ i=0
 
 echo -ne '' > square_wave.txt
 
-while [ $i -lt 128 ]; do
+while [ $i -lt 256 ]; do
   j=0
-  while [ $j -lt 32 ]; do
+  while [ $j -lt 16 ]; do
     echo -ne '\x7F\xFF' >> square_wave.txt
     j=$(( $j+1 ))
   done
   j=0
-  while [ $j -lt 32 ]; do
+  while [ $j -lt 16 ]; do
     echo -ne '\x00\x00' >> square_wave.txt
     j=$(( $j+1 ))
   done
